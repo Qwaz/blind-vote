62ef14f6756f0471b6ee936c901238b69c17a0ea
Qwaz <qwazpia@gmail.com>
2015-12-10 18:02:10 +0900

    master key generation

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+import os, binascii
 from flask import Flask
+from models import db, Setting
 
 app = Flask(__name__)
+app.config.from_object('config')
+db.init_app(app)
+
+
+def generate_key():
+    return binascii.hexlify(os.urandom(10)).decode('ascii')
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    if Setting.query.filter_by(name='master').first():
+        return 'Master Key Initialized'
+    else:
+        master = Setting('master', generate_key())
+        db.session.add(master)
+        db.session.commit()
+        return 'Master Key is %s' % master.value
 
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    with app.app_context():
+        db.create_all()
+    app.run('0.0.0.0', port=8080)
---
+++ models.py
@@ -0,0 +1,15 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Setting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    value = db.Column(db.String(50))
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __repr(self):
+        return '<Setting %s>' % self.name
